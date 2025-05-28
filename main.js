0c44f3652c49931018bd2fbabaa102c66d7692d4
sattorovshoxrux3009 <sattorovshoxrux3009@gmail.com>
2025-05-29 01:11:31 +0500

    Changed

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    frame: false, // Yuqori title bar'ni olib tashlaydi
+    fullscreen: true, // To‘liq ekran rejimi
+    autoHideMenuBar: true, // Menyu barni yashiradi
+    webPreferences: {
+      contextIsolation: true,
+    },
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
